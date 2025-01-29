486e1c5f200d6677f35704c940ca67b709bfcebc
Samarth Dhananjay Mule <103748997+SamarthMule@users.noreply.github.com>
2025-01-29 18:42:02 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,10 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
- root: '.',  // Ensure this is set correctly
-  build: {
-    outDir: 'dist'  // Ensure output folder is correct
-  },
   server: {
     proxy: {
       '/api': 'http://localhost:5000',
