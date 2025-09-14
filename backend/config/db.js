388150f2916ff80da8ab56b97f606acf05cdfa2c
Vipul Singh <vipulsinghnitmz@gmail.com>
2025-09-14 22:32:54 +0530

    Add axios dependency and fix mongoose typo in backend

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,9 @@
-import mongeoos from 'mongoose';
+import mongoose from 'mongoose';
 import { ENV_VARS } from './envVars.js';
 
 export const connectDB = async () => {
     try {
-        const conn = await mongeoos.connect(ENV_VARS.MONGO_URI);
+        const conn = await mongoose.connect(ENV_VARS.MONGO_URI);
         console.log(`MongoDB connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(`Error connecting to MongoDB: ${error.message}`);
